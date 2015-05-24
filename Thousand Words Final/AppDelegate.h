a857ea47c0fb6aef352d0483b2c342373de19171
Marian Mihai Motroc <MacMini@192-168-0-102.rdsnet.ro>
2015-05-24 20:47:55 +0300

    Initial Commit

---
+++ Thousand Words Final/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Thousand Words Final
+//
+//  Created by Marian Mihai Motroc on 5/24/15.
+//  Copyright (c) 2015 Marian Mihai Motroc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
